a848110400775b656c9c6593eca1646e1beaa8e9
Mizar999 <mizar2502@aol.com>
2013-10-21 14:12:14 +0200

    + added layout to output (almost) all data

--- MagicSearch/src/de/mizi/magicsearch/output/MagicCardFragment.java
+++ MagicSearch/src/de/mizi/magicsearch/output/MagicCardFragment.java
@@ -19,7 +19,20 @@ public class MagicCardFragment extends Fragment
 		MagicCardData data = (MagicCardData)args.getParcelable(KEY_DATA);
 		
 		View rootView = inflater.inflate(R.layout.swipe_fragment, container, false);
-		((TextView) rootView.findViewById(R.id.debugout)).setText(data.id + " " + data.name);
+		((TextView) rootView.findViewById(R.id.cardIdOut)).setText("" + data.id);
+		((TextView) rootView.findViewById(R.id.nameOut)).setText(data.name);
+		((TextView) rootView.findViewById(R.id.costOut)).setText(data.cost);
+		((TextView) rootView.findViewById(R.id.convertedCostOut)).setText("" + data.convertedCost);
+		((TextView) rootView.findViewById(R.id.typesOut)).setText(data.types);
+		((TextView) rootView.findViewById(R.id.rulesOut)).setText(data.rulesText);
+		((TextView) rootView.findViewById(R.id.flavorOut)).setText(data.flavorText);
+		((TextView) rootView.findViewById(R.id.artistOut)).setText(data.artist);
+		((TextView) rootView.findViewById(R.id.powerOut)).setText(data.power);
+		((TextView) rootView.findViewById(R.id.toughnessOut)).setText(data.toughness);
+		((TextView) rootView.findViewById(R.id.expansionOut)).setText(data.expansion);
+		((TextView) rootView.findViewById(R.id.rarityOut)).setText(data.rarity.toString());
+		((TextView) rootView.findViewById(R.id.numberOut)).setText(data.number);
+		((TextView) rootView.findViewById(R.id.cardRulesOut)).setText(data.cardRules);
 		return rootView;
 	}
 }
