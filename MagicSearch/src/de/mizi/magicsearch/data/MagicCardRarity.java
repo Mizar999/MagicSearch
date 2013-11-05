42e9efac1ccb00cd37fa7743f725163935d37df7
Mizar999 <Mizar999@users.noreply.github.com>
2013-11-05 08:49:16 +0100

    + added javadoc commentary

--- MagicSearch/src/de/mizi/magicsearch/data/MagicCardRarity.java
+++ MagicSearch/src/de/mizi/magicsearch/data/MagicCardRarity.java
@@ -1,5 +1,8 @@
 package de.mizi.magicsearch.data;
 
+/**
+ * This enum represents the rarity of a magiccard.
+ */
 public enum MagicCardRarity
 {
 	COMMON("Common"),
@@ -10,18 +13,35 @@ public enum MagicCardRarity
 	SPECIAL("Special"),
 	NONE("none");
 	
+	/**
+	 * The string representation of the rarity value.
+	 */
 	private final String name;
 	
+	/**
+	 * Creates a rarity value.
+	 */
 	private MagicCardRarity(String name)
 	{
 		this.name = name;
 	}
 	
+	/**
+	 * Returns a printable string represntation of the rarity value
+	 * 
+	 * @return a printable string representation
+	 */
 	public String toString()
 	{
 		return name;
 	}
 	
+	/**
+	 * Transforms a string value into a matching rarity value.
+	 * 
+	 * @param rarity the string value that shall be transformed into a rarity value
+	 * @return the matching rarity value or the rarity value 'NONE', if no rarity value matches the string value
+	 */
 	public static MagicCardRarity stringToRarity(String rarity)
 	{
 		for(MagicCardRarity element: values())
