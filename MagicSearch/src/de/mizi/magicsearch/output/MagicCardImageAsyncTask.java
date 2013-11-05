b92d1bb1b68763f56d7c1545ef44af395ab6508c
Mizar999 <Mizar999@users.noreply.github.com>
2013-11-05 11:24:25 +0100

    + added javadoc commentary

--- MagicSearch/src/de/mizi/magicsearch/output/MagicCardImageAsyncTask.java
+++ MagicSearch/src/de/mizi/magicsearch/output/MagicCardImageAsyncTask.java
@@ -13,13 +13,35 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+/**
+ * This asynchrinus task downloads a cardimage of a magiccard.
+ */
 public class MagicCardImageAsyncTask extends AsyncTask<String, String, Boolean>
 {
+	/**
+	 * This imageview shows the downloaded cardimage
+	 */
 	private ImageView image;
+	/**
+	 * The loading icon that is shown while the cardimage is downloaded.
+	 */
 	private ProgressBar progress;
+	/**
+	 * This textview shows an error message if something unexpected happened.
+	 */
 	private TextView text;
+	/**
+	 * The downloaded image data.
+	 */
 	private Bitmap bitmap;
 	
+	/**
+	 * The constructor creates the task object. It also sets all the references this object should have access to.
+	 * 
+	 * @param image reference to the imageview
+	 * @param progress reference to the loading icon
+	 * @param text reference to the textview for an error message
+	 */
 	public MagicCardImageAsyncTask(ImageView image, ProgressBar progress, TextView text)
 	{
 		this.image = image;
@@ -36,13 +58,14 @@ public class MagicCardImageAsyncTask extends AsyncTask<String, String, Boolean>
 		{
 			try
 			{
+				// Get access to the url from the given parameter and download the image data.
 				URL url = new URL(params[0]);
-	            HttpURLConnection conn = (HttpURLConnection) url.openConnection();   
-	            conn.setDoInput(true);   
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();   
+				conn.setDoInput(true);   
 				conn.connect();
-	            InputStream is = conn.getInputStream();
-	            bitmap = BitmapFactory.decodeStream(is); 
-	            success = true;
+				InputStream is = conn.getInputStream();
+				bitmap = BitmapFactory.decodeStream(is); 
+				success = true;
 			}
 			catch (IOException e) {
 				e.printStackTrace();
@@ -55,6 +78,7 @@ public class MagicCardImageAsyncTask extends AsyncTask<String, String, Boolean>
 	@Override
 	protected void onPostExecute(Boolean success)
 	{
+		// Hide the loading icon and show the image if everything went well or show an error message if something went wrong.
 		progress.setVisibility(View.GONE);
 		
 		if(success) {
